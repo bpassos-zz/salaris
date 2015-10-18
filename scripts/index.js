b25754a0b7708425b16d3a469167fabd23433ecd
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-10-17 22:52:41 -0300

    feat(#2): Starting issue

--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,96 @@
-document.onreadystatechange = function() {
+import calculate from './helpers/calculate';
 
-    document.getElementById('submit').onclick = function() {
+export default new class Salaris {
+  constructor() {
+    this.prepare();
+    this.bind();
+  }
 
-        submitForm();
-    }
+  prepare = () => {
+    this.elements = {};
+    this.elements.form = document.querySelector('[data-component="form"]');
+    this.elements.errorMessage = document.querySelector('[data-component="error-message"]');
+  }
 
-    function validateForm(grossSalary) {
+  bind = () => {
+    this.elements.form.addEventListener('submit', ::this.onFormSubmit, true);
+  }
 
-        var formValidation = document.getElementById('form-validaion-js');
-        if (grossSalary == null || grossSalary == "") {
-            formValidation.style.display = "block";
-            formValidation.innerHTML = 'We need to known your gross salary';
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    debugger;
+    console.log('wow', event);
+  }
+}
 
-            return false;
-        } else {
-            formValidation.style.display = "none";
-        }
-    }
-
-    function submitForm() {
-
-        var grossSalary = document.getElementById('gross_salary').value;
-        // var options = {
-        //     grossSalary: grossSalary,
-        // }
-        validateForm(grossSalary);
-
-        var result = document.getElementById('results');
-
-        // Check if the gross salary field is emputy
-        
-        var isThirtyPercent = document.getElementsByName('thirty_percent')[0].checked;
-
-        var currentDomNode;
-
-        var monthlySalary;
-        var year = 12;
-
-        var grossLittle = 19645;
-        var grossMedium = 33363;
-        var grossLarge = 55991;
-
-        var thrityPercentRullingMonth;
-
-        // Check to see if they user has 30% rulling
-        if(isThirtyPercent) {
-            thrityPercentRullingMonth = (grossSalary / year) * .25;
-            console.log('user has 30%');
-        } else if(grossSalary <= grossLittle) {
-            console.log('taxed 37%');
-            thrityPercentRullingMonth = (grossSalary / year) * .37;
-        } else {
-            console.log('taxed 42% - you earn a lot!');
-            thrityPercentRullingMonth = (grossSalary / year) * .42;
-        }
-
-        monthlySalary = (grossSalary / year) - thrityPercentRullingMonth; 
-        result.innerHTML = 'Your salary will be: <strong>&#8364;' + monthlySalary + '<strong>';
-        
-    }
-
-    document.onkeydown = function(){
-        if(window.event.keyCode == '13'){
-            console.log('enter!');
-            submitForm();
-        }
-    }
-} 
+// NOTE: refactor into "modules"
+// document.onreadystatechange = function() {
+// 
+//     document.getElementById('submit').onclick = function() {
+// 
+//         submitForm();
+//     }
+// 
+//     function validateForm(grossSalary) {
+// 
+//         var formValidation = document.getElementById('form-validaion-js');
+//         if (grossSalary == null || grossSalary == "") {
+//             formValidation.style.display = "block";
+//             formValidation.innerHTML = 'We need to known your gross salary';
+// 
+//             return false;
+//         } else {
+//             formValidation.style.display = "none";
+//         }
+//     }
+// 
+//     function submitForm() {
+// 
+//         var grossSalary = document.getElementById('gross_salary').value;
+//         // var options = {
+//         //     grossSalary: grossSalary,
+//         // }
+//         validateForm(grossSalary);
+// 
+//         var result = document.getElementById('results');
+// 
+//         // Check if the gross salary field is emputy
+//         
+//         var isThirtyPercent = document.getElementsByName('thirty_percent')[0].checked;
+// 
+//         var currentDomNode;
+// 
+//         var monthlySalary;
+//         var year = 12;
+// 
+//         var grossLittle = 19645;
+//         var grossMedium = 33363;
+//         var grossLarge = 55991;
+// 
+//         var thrityPercentRullingMonth = calculate(grossSalary, isThirtyPercent);
+// 
+// 
+//         // Check to see if they user has 30% rulling
+//         // if(isThirtyPercent) {
+//         //     thrityPercentRullingMonth = (grossSalary / year) * .25;
+//         //     console.log('user has 30%');
+//         // } else if(grossSalary <= grossLittle) {
+//         //     console.log('taxed 37%');
+//         //     thrityPercentRullingMonth = (grossSalary / year) * .37;
+//         // } else {
+//         //     console.log('taxed 42% - you earn a lot!');
+//         //     thrityPercentRullingMonth = (grossSalary / year) * .42;
+//         // }
+// 
+//         monthlySalary = (grossSalary / year) - thrityPercentRullingMonth; 
+//         result.innerHTML = 'Your salary will be: <strong>&#8364;' + monthlySalary + '<strong>';
+//         
+//     }
+// 
+//     document.onkeydown = function(){
+//         if(window.event.keyCode == '13'){
+//             console.log('enter!');
+//             submitForm();
+//         }
+//     }
+// } 
