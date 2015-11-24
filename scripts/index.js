b824e9017280aa776c1d10000c3ebb55948d3c33
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-11-24 10:35:31 -0200

    feat(script): Rendering result into the dom
    After this "split" I was not rendering the result into the dom...
    Now I'm using the "result" component in order to do that.

--- scripts/index.js
+++ scripts/index.js
@@ -20,8 +20,14 @@ export default new class Salaris {
   onFormSubmit(event) {
     event.preventDefault();
     let data = this.getData();
+    let value = this.formatResult(calculate(data));
 
-    console.log(calculate(data));
+    this.renderResult(value);
+    this.showResult();
+  }
+
+  formatResult(value) {
+    return `€ ${value}`;
   }
 
   getData() {
@@ -32,87 +38,28 @@ export default new class Salaris {
   }
 
   getGrossSalary() {
-    return this.elements.form.querySelector('[data-component-child="gross-salary"]').value;
+    return this.elements.form
+      .querySelector('[data-component-child="gross-salary"]')
+      .value;
   }
 
   isThirtyPercent() {
-    return this.elements.form.querySelector('[name="thirty_percent"]').checked;
+    return this
+      .elements
+      .form
+      .querySelector('[name="thirty_percent"]')
+      .checked;
   }
 
   renderResult(value) {
-    this.elements.result.innerHTML = value;
+    this.elements.result
+      .querySelector('[data-component-child="result__value"]')
+      .innerHTML = value;
+  }
+
+  showResult() {
+    this.elements.result.classList.remove('h-is-hidden');
   }
 
 }
 
-// NOTE: refactor into "modules"
-// document.onreadystatechange = function() {
-// 
-//     document.getElementById('submit').onclick = function() {
-// 
-//         submitForm();
-//     }
-// 
-//     function validateForm(grossSalary) {
-// 
-//         var formValidation = document.getElementById('form-validaion-js');
-//         if (grossSalary == null || grossSalary == "") {
-//             formValidation.style.display = "block";
-//             formValidation.innerHTML = 'We need to known your gross salary';
-// 
-//             return false;
-//         } else {
-//             formValidation.style.display = "none";
-//         }
-//     }
-// 
-//     function submitForm() {
-// 
-//         var grossSalary = document.getElementById('gross_salary').value;
-//         // var options = {
-//         //     grossSalary: grossSalary,
-//         // }
-//         validateForm(grossSalary);
-// 
-//         var result = document.getElementById('results');
-// 
-//         // Check if the gross salary field is emputy
-//         
-//         var isThirtyPercent = document.getElementsByName('')[0].checked;
-// 
-//         var currentDomNode;
-// 
-//         var monthlySalary;
-//         var year = 12;
-// 
-//         var grossLittle = 19645;
-//         var grossMedium = 33363;
-//         var grossLarge = 55991;
-// 
-//         var thrityPercentRullingMonth = calculate(grossSalary, isThirtyPercent);
-// 
-// 
-//         // Check to see if they user has 30% rulling
-//         // if(isThirtyPercent) {
-//         //     thrityPercentRullingMonth = (grossSalary / year) * .25;
-//         //     console.log('user has 30%');
-//         // } else if(grossSalary <= grossLittle) {
-//         //     console.log('taxed 37%');
-//         //     thrityPercentRullingMonth = (grossSalary / year) * .37;
-//         // } else {
-//         //     console.log('taxed 42% - you earn a lot!');
-//         //     thrityPercentRullingMonth = (grossSalary / year) * .42;
-//         // }
-// 
-//         monthlySalary = (grossSalary / year) - thrityPercentRullingMonth; 
-//         result.innerHTML = 'Your salary will be: <strong>&#8364;' + monthlySalary + '<strong>';
-//         
-//     }
-// 
-//     document.onkeydown = function(){
-//         if(window.event.keyCode == '13'){
-//             console.log('enter!');
-//             submitForm();
-//         }
-//     }
-// } 
